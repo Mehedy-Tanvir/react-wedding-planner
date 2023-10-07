a22cdd51f94f379f7f1940fd02fdebf5ee0bc923
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2023-10-07 17:51:04 +0600

    service card ui improved

--- src/Components/Card_Service/CardService.jsx
+++ src/Components/Card_Service/CardService.jsx
@@ -1,8 +1,9 @@
 import PropTypes from "prop-types";
 import { BsFillArrowRightCircleFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
 const CardService = ({ service }) => {
   return (
-    <div className="relative shadow-xl card bg-base-100">
+    <div className="shadow-xl card bg-base-100">
       <div>
         <figure>
           <img
@@ -11,19 +12,31 @@ const CardService = ({ service }) => {
             alt="image"
           />
         </figure>
-        <div className="card-body">
-          <h2 className="card-title">{service?.title}</h2>
-          <p>{service?.description_short}</p>
+        <div className="flex flex-col h-[320px] card-body">
+          <div className="flex-grow">
+            <h2 className="card-title">{service?.title}</h2>
+
+            <p>{service?.description_short}</p>
+            <p className="text-[#6C2C71] text-2xl">Price: ${service?.price}</p>
+          </div>
+          <Link to={`/service/${service?.id}`}>
+            <div className="bg-[#6C2C71] cursor-pointer hover:opacity-90 p-4">
+              <button className="flex items-center justify-center w-full gap-3 text-xl text-white normal-case">
+                <span>Show Details</span>
+                <BsFillArrowRightCircleFill className="text-[24px] text-white"></BsFillArrowRightCircleFill>
+              </button>
+            </div>
+          </Link>
         </div>
       </div>
-      <div className="h-full opacity-0 hover:opacity-100 transition-opacity duration-300 ease-in-out bg-[#6C2C7080] w-full absolute z-20 rounded-xl">
+      {/* <div className="h-full opacity-0 hover:opacity-100 transition-opacity duration-300 ease-in-out bg-[#6C2C7080] w-full absolute z-20 rounded-xl">
         <div className="flex flex-col items-center justify-center w-full h-full">
-          <h1 className="text-[40px] text-white">See Details</h1>
+          <h1 className="text-[40px] font-work-sans text-white">See Details</h1>
           <div>
             <BsFillArrowRightCircleFill className="text-[40px] text-white"></BsFillArrowRightCircleFill>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,7 +7,10 @@ const Home = () => {
   return (
     <div>
       <Banner></Banner>
-      <div className="grid grid-cols-1 gap-6 px-2 pt-6 md:grid-cols-2 lg:grid-cols-3">
+      <h1 className="mt-10 text-5xl font-semibold text-center md:mt-20 bold font-sans-serif">
+        Our Services
+      </h1>
+      <div className="grid grid-cols-1 gap-6 px-2 pt-6 md:mt-10 md:grid-cols-2 lg:grid-cols-3">
         {data?.map((service) => (
           <CardService key={service.id} service={service}></CardService>
         ))}
