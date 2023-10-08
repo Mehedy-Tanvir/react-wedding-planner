dedac60aeed7592af95a37234dff5be27970c77d
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2023-10-08 18:47:29 +0600

    error page added

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom";
+
 const Error = () => {
   return (
-    <div>
-      <h1>Error 404! Page not found!</h1>
+    <div className="flex flex-col items-center justify-center h-screen gap-6">
+      <img src="/logo-dark.png" alt="" />
+      <h1 className="text-4xl font-bold text-center text-[#0B0B0BB3]">
+        Error 404! Page not found!
+      </h1>
+      <Link to="/">
+        <button className="bg-[#6C2C71] hover:opacity-90 text-white text-2xl font-semibold h-[60px] px-[20px] rounded-none">
+          Go Home
+        </button>
+      </Link>
     </div>
   );
 };
